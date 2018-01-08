8bfe69167d1671d6316c0571456646c439c52573
krzysztof.siejkowski <krzysztof.siejkowski@polidea.com>
2018-01-08 16:49:13 +0100

    POC of renaming and project cleanup
    Reviewers: jakub.sowa, jerzy.kleszcz
    
    Reviewed By: jakub.sowa, jerzy.kleszcz
    
    Subscribers: jakub.sowa, jerzy.kleszcz
    
    Differential Revision: https://phabricator.polidea.com/D2739

--- swift/include/swift/Obfuscation/Random.h
+++ swift/include/swift/Obfuscation/Random.h
@@ -1,83 +1,92 @@
 #ifndef Random_h
 #define Random_h
 
-#include <stdio.h>
+#include <vector>
+#include <string>
 #include <random>
 
 namespace swift {
+namespace obfuscation {
+
+class RandomIntegerGenerator {
   
-  namespace obfuscation {
-    
-    class RandomIntegerGenerator {
-      
-    private:
-      
-      std::random_device rd;
-      std::mt19937 engine;
-      std::uniform_int_distribution<size_t> distribution;
-      
-    public:
-      
-      RandomIntegerGenerator(size_t min, size_t max) :
-      rd(),
-      engine(rd()),
-      distribution(std::uniform_int_distribution<size_t>(min, max)) {}
-      
-      size_t rand();
-
-    };
-    
-    template<typename T>
-    class RandomElementChooser {
-      
-    private:
-      
-      RandomIntegerGenerator* generator;
-      std::vector<T> list;
-      
-    public:
-      
-      RandomElementChooser(std::vector<T> listToChooseFrom) :
-      generator(new RandomIntegerGenerator(0, listToChooseFrom.size() - 1)),
-      list(listToChooseFrom) {}
-      
-      T rand();
-
-    };
-    
-    template<typename T>
-    class RandomVectorGenerator {
-      
-    private:
-      
-      RandomElementChooser<T>* chooser;
-      
-    public:
-      
-      RandomVectorGenerator(std::vector<T> listToChooseFrom) :
-      chooser(new RandomElementChooser<T>(listToChooseFrom)) {}
-      
-      std::vector<T> rand(size_t length);
-
-    };
-    
-    class RandomStringGenerator {
-      
-    private:
-      
-      RandomVectorGenerator<std::string>* generator;
-      
-    public:
-      
-      RandomStringGenerator(std::vector<std::string> listToChooseFrom) :
-      generator(new RandomVectorGenerator<std::string>(listToChooseFrom)) {}
-      
-      std::string rand(size_t length);
-
-    };
-    
-  } //namespace obfuscation
+private:
   
+  std::random_device RandomDevice;
+  std::mt19937 Engine;
+  std::uniform_int_distribution<int> Distribution;
+  
+public:
+  
+  RandomIntegerGenerator(int Min, int Max)
+  : RandomDevice(),
+  Engine(RandomDevice()),
+  Distribution(std::uniform_int_distribution<int>(Min, Max)) {}
+  
+  int rand();
+
+};
+
+template<typename T>
+class RandomElementChooser {
+  
+private:
+  
+  RandomIntegerGenerator* Generator;
+  std::vector<T> List;
+  
+public:
+  
+  RandomElementChooser(const std::vector<T> &ListToChooseFrom) :
+  Generator(new RandomIntegerGenerator(0, ListToChooseFrom.size() - 1)),
+  List(ListToChooseFrom) {}
+  
+  T rand() const;
+  
+  ~RandomElementChooser();
+
+};
+
+template<typename T>
+class RandomVectorGenerator {
+  
+private:
+  
+  RandomElementChooser<T>* Chooser;
+  
+public:
+  
+  RandomVectorGenerator(const std::vector<T> &ListToChooseFrom) :
+  Chooser(new RandomElementChooser<T>(ListToChooseFrom)) {}
+  
+  typedef typename std::vector<T>::size_type size_type;
+  
+  std::vector<T> rand(size_type Length) const;
+  
+  ~RandomVectorGenerator();
+
+};
+
+class RandomStringGenerator {
+  
+private:
+  
+  RandomVectorGenerator<std::string>* Generator;
+  
+public:
+  
+  typedef std::vector<std::string>::size_type size_type;
+  
+  RandomStringGenerator(const std::vector<std::string> &ListToChooseFrom)
+  : Generator(new RandomVectorGenerator<std::string>(ListToChooseFrom)) {}
+  
+  std::string rand(size_type Length) const;
+  
+  ~RandomStringGenerator();
+
+};
+
+} //namespace obfuscation
 } //namespace swift
 
 #endif /* Random_h */
--- swift/lib/Obfuscation/FileIO.cpp
+++ swift/lib/Obfuscation/FileIO.cpp
@@ -1,68 +1,72 @@
 #include "swift/Obfuscation/FileIO.h"
-#include "llvm/Support/YAMLTraits.h"
-#include "llvm/Support/YAMLParser.h"
+#include "swift/Obfuscation/DataStructures.h"
+
+#include "llvm/Support/FileSystem.h"
 #include "llvm/Support/MemoryBuffer.h"
+#include "llvm/Support/YAMLParser.h"
 #include "swift/Basic/JSONSerialization.h"
-#include <stdio.h>
-#include <iostream>
-#include <fstream>
 
 namespace swift {
-  namespace obfuscation {
-        
-    template<class T>
-    llvm::ErrorOr<T> parseJson(std::string PathToJson) {
-      auto Buffer = llvm::MemoryBuffer::getFile(PathToJson);
-      if (auto ErrorCode = Buffer.getError()) {
-        std::cout << "Error during JSON file read: " << ErrorCode.message();
-        return llvm::ErrorOr<T>(std::error_code(1, std::generic_category()));
-      }
-      
-      llvm::yaml::Input Input(std::move(Buffer.get())->getBuffer());
-      T json;
-      Input >> json;
-      std::error_code ParsingError = Input.error();
-      if (ParsingError) {
-        std::cout << "Error during JSON parse: " << ParsingError.message();
-        return llvm::ErrorOr<T>(std::error_code(1, std::generic_category()));
-      }
-      
-      return llvm::ErrorOr<T>(json);
-    }
-    
-    template llvm::ErrorOr<FilesJson> parseJson(std::string);
-    
-    template llvm::ErrorOr<SymbolsJson> parseJson(std::string);
-    
-    template llvm::ErrorOr<RenamesJson> parseJson(std::string);
+namespace obfuscation {
 
-    template<class T>
-    int writeSymbolsToFile(T Symbols, std::string PathToOutput) {
-      
-      std::string outputStr;
-      llvm::raw_string_ostream rso(outputStr);
-      swift::json::Output output(rso);
-      output << Symbols;
-      // TODO: add serialization error handling
-      
-      std::ofstream file;
-      file.open(PathToOutput);
-      if (file.fail()) {
-        file.close();
-        std::cout << "Failed to open file: " << PathToOutput << "\n";
-        return 1;
-      }
-      file << rso.str();
-      file.close();
-      
-      std::cout << "symbols.json:\n" << rso.str();
-      
-      return 0;
-    }
-    
-    template int writeSymbolsToFile(SymbolsJson Symbols, std::string PathToOutput);
-    
-    template int writeSymbolsToFile(RenamesJson Symbols, std::string PathToOutput);
-    
-  } //namespace obfuscation
+template<class T>
+llvm::Expected<T> parseJson(std::string PathToJson) {
+  auto Buffer = llvm::MemoryBuffer::getFile(PathToJson);
+  if (auto ErrorCode = Buffer.getError()) {
+    return llvm::make_error<llvm::StringError>("Error during JSON file read",
+                                               ErrorCode);
+  }
+  
+  llvm::yaml::Input Input(std::move(Buffer.get())->getBuffer());
+  T Json;
+  Input >> Json;
+  if (auto ErrorCode = Input.error()) {
+    return llvm::make_error<llvm::StringError>("Error during JSON parse:",
+                                               ErrorCode);
+  }
+  
+  return Json;
+}
+
+template llvm::Expected<FilesJson> parseJson(std::string);
+
+template llvm::Expected<SymbolsJson> parseJson(std::string);
+
+template llvm::Expected<RenamesJson> parseJson(std::string);
+
+template<class T>
+llvm::Error writeToFile(T &Object,
+                        std::string PathToOutput,
+                        llvm::raw_ostream &LogStream) {
+  
+  std::string OutputString;
+  llvm::raw_string_ostream OutputStringStream(OutputString);
+  swift::json::Output Output(OutputStringStream);
+  Output << Object;
+  // TODO: add serialization error handling
+  
+  std::error_code Error;
+  llvm::raw_fd_ostream File(PathToOutput, Error, llvm::sys::fs::F_None);
+  if (File.has_error() || Error) {
+    auto Message = "Failed to open file: " + PathToOutput;
+    return llvm::make_error<llvm::StringError>(Message, Error);
+  }
+  File << OutputStringStream.str();
+  File.close();
+  
+  LogStream << "Written to file: " << '\n'
+    << OutputStringStream.str() << '\n';
+  
+  return llvm::Error::success();
+}
+
+template llvm::Error writeToFile(SymbolsJson &,
+                                 std::string,
+                                 llvm::raw_ostream &);
+
+template llvm::Error writeToFile(RenamesJson &,
+                                 std::string,
+                                 llvm::raw_ostream &);
+
+} //namespace obfuscation
 } //namespace swift
