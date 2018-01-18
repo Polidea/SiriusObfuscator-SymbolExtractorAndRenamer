eaa84209c6361ee44760fdcdb71a360a3528f076
krzysztof.siejkowski <krzysztof.siejkowski@polidea.com>
2018-01-18 14:09:20 +0100

    Basic support for function renaming
    Reviewers: jerzy.kleszcz, jakub.sowa
    
    Reviewed By: jerzy.kleszcz, jakub.sowa
    
    Subscribers: jakub.sowa, jerzy.kleszcz
    
    Differential Revision: https://phabricator.polidea.com/D2762

--- swift/lib/Obfuscation/Renaming.cpp
+++ swift/lib/Obfuscation/Renaming.cpp
@@ -20,7 +20,9 @@ computeObfuscatedPath(const StringRef Filename,
                       const StringRef OriginalProjectPath,
                       const StringRef ObfuscatedProjectPath) {
   SmallPath Path(Filename);
-  llvm::sys::path::replace_path_prefix(Path, OriginalProjectPath, ObfuscatedProjectPath);
+  llvm::sys::path::replace_path_prefix(Path,
+                                       OriginalProjectPath,
+                                       ObfuscatedProjectPath);
   return Path;
 }
 
@@ -28,7 +30,8 @@ llvm::Error copyProject(const StringRef OriginalPath,
                         const StringRef ObfuscatedPath) {
   
   std::error_code ErrorCode;
-  for (llvm::sys::fs::recursive_directory_iterator Iterator(OriginalPath, ErrorCode), End;
+  using RecursiveIterator = llvm::sys::fs::recursive_directory_iterator;
+  for (RecursiveIterator Iterator(OriginalPath, ErrorCode), End;
        Iterator != End && !ErrorCode;
        Iterator.increment(ErrorCode)) {
     if (llvm::sys::fs::is_directory(Iterator->path())) {
@@ -46,16 +49,21 @@ llvm::Error copyProject(const StringRef OriginalPath,
     auto DirectoryPath = Path;
     llvm::sys::path::remove_filename(DirectoryPath);
     if (auto Error = llvm::sys::fs::create_directories(DirectoryPath)) {
-      return stringError("Cannot create directory in " + Path.str().str(), Error);
+      auto Message = "Cannot create directory in " + Path.str().str();
+      return stringError(Message, Error);
     }
     
     if (auto Error = llvm::sys::fs::copy_file(Iterator->path(), Path)) {
-      return stringError("Cannot copy file from " + Iterator->path() + " to " + Path.str().str(), Error);
+      auto Message = "Cannot copy file from " + Iterator->path() + " to " +
+        Path.str().str();
+      return stringError(Message, Error);
     }
   }
   
   if (ErrorCode) {
-    return stringError("Error while traversing the project directory " + OriginalPath.str(), ErrorCode);
+    auto Message = "Error while traversing the project directory " +
+      OriginalPath.str();
+    return stringError(Message, ErrorCode);
   }
   
   return llvm::Error::success();
@@ -78,27 +86,33 @@ llvm::Expected<bool> performActualRenaming(SourceFile &Current,
   bool performedRenaming = false;
   auto SymbolsWithRanges = walkAndCollectSymbols(Current);
   
-  std::unique_ptr<llvm::raw_fd_ostream> DescriptorStream(nullptr);
-  std::unique_ptr<swift::ide::SourceEditOutputConsumer> Editor(nullptr);
+  using EditConsumer = swift::ide::SourceEditOutputConsumer;
   
-  //TODO: would be way better to have a map here instead of iterating through symbols
+  std::unique_ptr<llvm::raw_fd_ostream> DescriptorStream(nullptr);
+  std::unique_ptr<EditConsumer> Editor(nullptr);
+  
+  //TODO: would be way better to have a map instead of iterating through symbols
   for (const auto &SymbolWithRange : SymbolsWithRanges) {
     for (const auto &Symbol : RenamesJson.Symbols) {
       
       if (shouldRename(Symbol, SymbolWithRange, ModuleName)) {
         if (Editor == nullptr) {
           std::error_code Error;
-          DescriptorStream = llvm::make_unique<llvm::raw_fd_ostream>(Path, Error, llvm::sys::fs::F_None);
+          DescriptorStream =
+            llvm::make_unique<llvm::raw_fd_ostream>(Path,
+                                                    Error,
+                                                    llvm::sys::fs::F_None);
           if (DescriptorStream->has_error() || Error) {
             return stringError("Cannot open output file: " + Path.str(), Error);
           }
-          Editor = llvm::make_unique<swift::ide::SourceEditOutputConsumer>(SourceManager,
-                                                                           BufferId,
-                                                                           *DescriptorStream);
+          Editor = llvm::make_unique<EditConsumer>(SourceManager,
+                                                   BufferId,
+                                                   *DescriptorStream);
         }
+        auto ObfuscatedName = StringRef(Symbol.ObfuscatedName);
         Editor->ide::SourceEditConsumer::accept(SourceManager,
                                                 SymbolWithRange.Range,
-                                                StringRef(Symbol.ObfuscatedName));
+                                                ObfuscatedName);
         performedRenaming = true;
         break;
       }
@@ -118,7 +132,8 @@ performRenaming(std::string MainExecutablePath,
     return std::move(Error);
   }
   
-  if (auto Error = copyProject(FilesJson.Project.RootPath, ObfuscatedProjectPath)) {
+  if (auto Error = copyProject(FilesJson.Project.RootPath,
+                               ObfuscatedProjectPath)) {
     return std::move(Error);
   }
   
