d7dcf7ca306f7c607021aa77fc25e68b2c8832cb
krzysztof.siejkowski <krzysztof.siejkowski@polidea.com>
2018-01-22 15:15:26 +0100

    Support from overriden methods
    Reviewers: jakub.sowa, jerzy.kleszcz
    
    Reviewed By: jakub.sowa, jerzy.kleszcz
    
    Subscribers: jakub.sowa, jerzy.kleszcz
    
    Differential Revision: https://phabricator.polidea.com/D2771

--- swift/unittests/SwiftObfuscation/DataStructuresSerializationTests.cpp
+++ swift/unittests/SwiftObfuscation/DataStructuresSerializationTests.cpp
@@ -5,7 +5,7 @@ using namespace swift::json;
 using namespace llvm;
 
 TEST(DataStructuresSerialization, SerializeSymbol) {
-  Symbol Object("sampleIdentifier", "sampleName", "sampleModule");
+  Symbol Object("sampleIdentifier", "sampleName", "sampleModule", SymbolType::Type);
   
   auto Serialized = serialize(Object);
   
@@ -13,19 +13,22 @@ TEST(DataStructuresSerialization, SerializeSymbol) {
   "{\n"
   "  \"name\": \"sampleName\",\n"
   "  \"identifier\": \"sampleIdentifier\",\n"
-  "  \"module\": \"sampleModule\"\n"
+  "  \"module\": \"sampleModule\",\n"
+  "  \"type\": \"type\"\n"
   "}";
   
   EXPECT_EQ(Serialized, Expected);
 }
 
 TEST(DataStructuresSerialization, SerializeSymbolsJson) {
-  Symbol Symbol0("sampleIdentifier0", "sampleName0", "sampleModule0");
-  Symbol Symbol1("sampleIdentifier1", "sampleName1", "sampleModule1");
+  Symbol Symbol0("sampleIdentifier0", "sampleName0", "sampleModule0", SymbolType::Type);
+  Symbol Symbol1("sampleIdentifier1", "sampleName1", "sampleModule1", SymbolType::NamedFunction);
+  Symbol Symbol2("sampleIdentifier2", "sampleName2", "sampleModule2", SymbolType::Operator);
   
   auto Object = SymbolsJson();
   Object.Symbols.push_back(Symbol0);
   Object.Symbols.push_back(Symbol1);
+  Object.Symbols.push_back(Symbol2);
   
   auto Serialized = serialize(Object);
   
@@ -35,11 +38,19 @@ TEST(DataStructuresSerialization, SerializeSymbolsJson) {
   "    {\n"
   "      \"name\": \"sampleName0\",\n"
   "      \"identifier\": \"sampleIdentifier0\",\n"
-  "      \"module\": \"sampleModule0\"\n"
+  "      \"module\": \"sampleModule0\",\n"
+  "      \"type\": \"type\"\n"
   "    },\n"
-  "    {\n      \"name\": \"sampleName1\",\n"
+  "    {\n"
+  "      \"name\": \"sampleName1\",\n"
   "      \"identifier\": \"sampleIdentifier1\",\n"
-  "      \"module\": \"sampleModule1\"\n"
+  "      \"module\": \"sampleModule1\",\n"
+  "      \"type\": \"namedFunction\"\n"
+  "    },\n"
+  "    {\n      \"name\": \"sampleName2\",\n"
+  "      \"identifier\": \"sampleIdentifier2\",\n"
+  "      \"module\": \"sampleModule2\",\n"
+  "      \"type\": \"operator\"\n"
   "    }\n"
   "  ]\n"
   "}";
@@ -48,7 +59,11 @@ TEST(DataStructuresSerialization, SerializeSymbolsJson) {
 }
 
 TEST(DataStructuresSerialization, SerializeSymbolRenaming) {
-  SymbolRenaming Object("sampleIdentifier", "sampleName", "sampleObfuscatedName", "sampleModule");
+  SymbolRenaming Object("sampleIdentifier",
+                        "sampleName",
+                        "sampleObfuscatedName",
+                        "sampleModule",
+                        SymbolType::Type);
   
   auto Serialized = serialize(Object);
   
@@ -57,7 +72,8 @@ TEST(DataStructuresSerialization, SerializeSymbolRenaming) {
   "  \"identifier\": \"sampleIdentifier\",\n"
   "  \"originalName\": \"sampleName\",\n"
   "  \"obfuscatedName\": \"sampleObfuscatedName\",\n"
-  "  \"module\": \"sampleModule\"\n"
+  "  \"module\": \"sampleModule\",\n"
+  "  \"type\": \"type\"\n"
   "}";
   
   EXPECT_EQ(Serialized, Expected);
@@ -65,12 +81,14 @@ TEST(DataStructuresSerialization, SerializeSymbolRenaming) {
 
 
 TEST(DataStructuresSerialization, SerializeRenamesJson) {
-  SymbolRenaming Symbol0("sampleIdentifier0", "sampleName0", "sampleObfuscatedName0", "sampleModule0");
-  SymbolRenaming Symbol1("sampleIdentifier1", "sampleName1", "sampleObfuscatedName1", "sampleModule1");
+  SymbolRenaming Symbol0("sampleIdentifier0", "sampleName0", "sampleObfuscatedName0", "sampleModule0", SymbolType::Type);
+  SymbolRenaming Symbol1("sampleIdentifier1", "sampleName1", "sampleObfuscatedName1", "sampleModule1", SymbolType::NamedFunction);
+  SymbolRenaming Symbol2("sampleIdentifier2", "sampleName2", "sampleObfuscatedName2", "sampleModule2", SymbolType::Operator);
   
   auto Object = RenamesJson();
   Object.Symbols.push_back(Symbol0);
   Object.Symbols.push_back(Symbol1);
+  Object.Symbols.push_back(Symbol2);
   
   auto Serialized = serialize(Object);
   
@@ -81,13 +99,22 @@ TEST(DataStructuresSerialization, SerializeRenamesJson) {
   "      \"identifier\": \"sampleIdentifier0\",\n"
   "      \"originalName\": \"sampleName0\",\n"
   "      \"obfuscatedName\": \"sampleObfuscatedName0\",\n"
-  "      \"module\": \"sampleModule0\"\n"
+  "      \"module\": \"sampleModule0\",\n"
+  "      \"type\": \"type\"\n"
   "    },\n"
   "    {\n"
   "      \"identifier\": \"sampleIdentifier1\",\n"
   "      \"originalName\": \"sampleName1\",\n"
   "      \"obfuscatedName\": \"sampleObfuscatedName1\",\n"
-  "      \"module\": \"sampleModule1\"\n"
+  "      \"module\": \"sampleModule1\",\n"
+  "      \"type\": \"namedFunction\"\n"
+  "    },\n"
+  "    {\n"
+  "      \"identifier\": \"sampleIdentifier2\",\n"
+  "      \"originalName\": \"sampleName2\",\n"
+  "      \"obfuscatedName\": \"sampleObfuscatedName2\",\n"
+  "      \"module\": \"sampleModule2\",\n"
+  "      \"type\": \"operator\"\n"
   "    }\n"
   "  ]\n"
   "}";
