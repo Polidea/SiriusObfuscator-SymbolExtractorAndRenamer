611f84d2fdac1f6694d5da637424476dc2c068c4
krzysztof.siejkowski <krzysztof.siejkowski@polidea.com>
2018-01-25 16:21:39 +0100

    Parameters obfuscation
    Reviewers: jerzy.kleszcz, jakub.sowa
    
    Reviewed By: jerzy.kleszcz, jakub.sowa
    
    Subscribers: jakub.sowa, jerzy.kleszcz
    
    Differential Revision: https://phabricator.polidea.com/D2776

--- swift/lib/Obfuscation/DeclarationParser.cpp
+++ swift/lib/Obfuscation/DeclarationParser.cpp
@@ -1,60 +1,17 @@
 #include "swift/Obfuscation/DeclarationParser.h"
+#include "swift/Obfuscation/FunctionDeclarationParser.h"
+#include "swift/Obfuscation/ParameterDeclarationParser.h"
 #include "swift/Obfuscation/Utils.h"
-
-#include <sstream>
-#include <string>
-#include <vector>
 
 namespace swift {
 namespace obfuscation {
-
-std::string combineIdentifier(std::vector<std::string> &Parts) {
-  if (Parts.empty()) {
-    return "";
-  } else if (Parts.size() == 1) {
-    return Parts[0];
-  } else {
-    //TODO: can we rewrite it to use llvm:raw_string_ostream?
-    std::stringstream ResultStream;
-    std::copy(Parts.cbegin(),
-              Parts.cend(),
-              std::ostream_iterator<std::string>(ResultStream, "."));
-    std::string Result = ResultStream.str();
-    Result.pop_back();
-    return Result;
-  }
-}
   
-std::string moduleName(const Decl* Declaration) {
-  return Declaration->getModuleContext()->getBaseName().getIdentifier().get();
-}
-
-std::pair<std::string, std::vector<std::string>>
-moduleNameAndParts(const Decl *Declaration) {
-  std::vector<std::string> Parts;
-  std::string ModuleName = moduleName(Declaration);
-  Parts.push_back("module");
-  Parts.push_back(ModuleName);
-  return std::make_pair(ModuleName, Parts);
-}
+using SingleSymbolOrError = llvm::Expected<Symbol>;
   
-std::string typeName(const NominalTypeDecl* Declaration) {
-  return Declaration->getDeclaredInterfaceType()->getString();
-}
-  
-llvm::Expected<std::string>
-enclosingTypeName(const Decl* Declaration) {
-  auto *DeclarationContext = Declaration->getDeclContext();
-  if (auto *TypeDeclaration =
-        DeclarationContext->getAsNominalTypeOrNominalTypeExtensionContext()) {
-    return typeName(TypeDeclaration);
-  }
-  return stringError("enclosing context of this declaration is not supported");
-}
-  
-llvm::Expected<Symbol> parse(const NominalTypeDecl* Declaration) {
+SingleSymbolOrError parse(const NominalTypeDecl* Declaration) {
   
   auto ModuleNameAndParts = moduleNameAndParts(Declaration);
+  std::string ModuleName = ModuleNameAndParts.first;
   std::vector<std::string> Parts = ModuleNameAndParts.second;
   std::string SymbolName = typeName(Declaration);
   
@@ -72,146 +29,75 @@ llvm::Expected<Symbol> parse(const NominalTypeDecl* Declaration) {
   
   return Symbol(combineIdentifier(Parts),
                 SymbolName,
-                ModuleNameAndParts.first,
+                ModuleName,
                 SymbolType::Type);
 }
-  
-std::string functionName(const FuncDecl* Declaration) {
-  return Declaration->getName().str().str();
-}
-  
-llvm::Error isDeclarationSupported(const FuncDecl* Declaration) {
-  if (Declaration->isBinaryOperator() || Declaration->isUnaryOperator()) {
-    return stringError("don't support operators right now, since it requires "
-                       "the special obfuscated identifier");
-  }
-  if (Declaration->isGetterOrSetter()) {
-    return stringError("don't support getters and setters right now, since "
-                       "it's the computed property name that should be "
-                       "obfuscated");
-  }
-  if (Declaration->isAccessor() || Declaration->isObservingAccessor()) {
-    return stringError("don't support property accessors right now");
-  }
-  return llvm::Error::success();
-}
-  
-const FuncDecl*
-baseOverridenDeclarationWithModules(const FuncDecl* Declaration,
-                                    std::set<std::string> &Modules) {
-  if (auto* OverrideDeclaration = Declaration->getOverriddenDecl()) {
-    Modules.insert(moduleName(OverrideDeclaration));
-    return baseOverridenDeclarationWithModules(OverrideDeclaration, Modules);
-  } else {
-    return Declaration;
-  }
-}
-  
-Symbol symbolFromFunctionDeclaration(const swift::FuncDecl *Declaration,
-                                     std::string &ModuleName,
-                                     std::vector<std::string> &Parts) {
-  
-  std::string SymbolName = functionName(Declaration);
-  
-  auto ProtocolRequirements = Declaration->getSatisfiedProtocolRequirements();
-  auto *ProtocolDeclaration =
-    dyn_cast<ProtocolDecl>(Declaration->getDeclContext());
-  if (ProtocolRequirements.empty() && ProtocolDeclaration == nullptr) {
-    
-    auto TypeNameOrError = enclosingTypeName(Declaration);
-    if (auto Error = TypeNameOrError.takeError()) {
-      llvm::consumeError(std::move(Error));
-      Parts.push_back("function." + SymbolName);
-    } else {
-      Parts.push_back("type." + TypeNameOrError.get());
-      Parts.push_back("method." + SymbolName);
-    }
-    
-  } else {
-    
-    // TODO: right now we're renaming all the methods in the protocols with
-    //       the same name and signature to the same obfuscated name. the reason
-    //       is that it greatly simplifies the handling of situation in which
-    //       a protocol A has method a() and protocol B: A has method a().
-    //       then the class C: B with the implementation of a() satisfies
-    //       two requirements at the same time (from A and from B). however,
-    //       there is no override relationship between the A.a() and B.a() in
-    //       protocols. it's just a name that's the same.
-    //       this simplified handling should be improved in the future.
-    if (!ProtocolRequirements.empty()) {
-      ModuleName = moduleName(ProtocolRequirements.front());
-    } else {
-      ModuleName = moduleName(ProtocolDeclaration);
-    }
-    Parts.push_back("protocol");
-    Parts.push_back("method." + SymbolName);
-  }
-  
-  if (Declaration->getDeclContext()->isTypeContext()) {
-    auto Interface = Declaration->getMethodInterfaceType().getString();
-    Parts.push_back("signature." + Interface);
-  } else {
-    Parts.push_back("signature." + Declaration->getInterfaceType().getString());
-  }
-  
-  return Symbol(combineIdentifier(Parts),
-                SymbolName,
-                ModuleName,
-                SymbolType::NamedFunction);
-}
-  
-llvm::Expected<Symbol> parse(const FuncDecl* Declaration) {
 
-  if (auto Error = isDeclarationSupported(Declaration)) {
-    return std::move(Error);
+SingleSymbolOrError parse(const VarDecl* Declaration) {
+  
+  if (Declaration->getOverriddenDecl() != nullptr) {
+    return stringError("obfuscating overriding properties is not supported");
   }
   
   auto ModuleNameAndParts = moduleNameAndParts(Declaration);
   std::string ModuleName = ModuleNameAndParts.first;
   std::vector<std::string> Parts = ModuleNameAndParts.second;
+  std::string SymbolName = Declaration->getName().str().str();
   
-  if (Declaration->getOverriddenDecl() != nullptr) {
-    
-    std::set<std::string> Modules;
-    auto Base = baseOverridenDeclarationWithModules(Declaration, Modules);
-    
-    if (Modules.size() == 1 && Modules.count(ModuleName) == 1) {
-      
-      return symbolFromFunctionDeclaration(Base, ModuleName, Parts);
-      
-    } else {
-      return stringError("only method overriding methods from the same module "
-                         "might be safely obfuscated");
-    }
-    
+  if (auto *FunctionDeclaration =
+      dyn_cast<FuncDecl>(Declaration->getDeclContext())) {
+    std::string FunctionName = functionName(FunctionDeclaration);
+    auto ModuleAndParts = functionIdentifierParts(FunctionDeclaration,
+                                                  ModuleName,
+                                                  FunctionName);
+    copyToVector(ModuleAndParts.second, Parts);
   }
-  return symbolFromFunctionDeclaration(Declaration, ModuleName, Parts);
+  
+  Parts.push_back("variable." + SymbolName);
+  
+  return Symbol(combineIdentifier(Parts),
+                SymbolName,
+                ModuleName,
+                SymbolType::Variable);
 }
 
-llvm::Expected<Symbol> parse(const ValueDecl* Declaration) {
-  return stringError("found unsupported declaration type");
-}
-
-llvm::Expected<Symbol> extractSymbol(Decl* Declaration) {
-  std::unique_ptr<llvm::Expected<Symbol>> SymbolOrError(nullptr);
+SymbolsOrError extractSymbol(Decl* Declaration, CharSourceRange Range) {
+  
+  std::unique_ptr<SingleSymbolOrError> SingleSymbolOrErrorPointer(nullptr);
+  std::unique_ptr<SymbolsOrError> SymbolsOrErrorPointer(nullptr);
+  
   if (const auto *NominalTypeDeclaration =
         dyn_cast<NominalTypeDecl>(Declaration)) {
-    SymbolOrError =
-      llvm::make_unique<llvm::Expected<Symbol>>(parse(NominalTypeDeclaration));
+    SingleSymbolOrErrorPointer =
+      llvm::make_unique<SingleSymbolOrError>(parse(NominalTypeDeclaration));
   } else if (const auto *FuncDeclaration = dyn_cast<FuncDecl>(Declaration)) {
-    SymbolOrError =
-      llvm::make_unique<llvm::Expected<Symbol>>(parse(FuncDeclaration));
-  } else if (const auto *ValueDeclaration = dyn_cast<ValueDecl>(Declaration)) {
-    SymbolOrError =
-      llvm::make_unique<llvm::Expected<Symbol>>(parse(ValueDeclaration));
+    SymbolsOrErrorPointer =
+      llvm::make_unique<SymbolsOrError>(parse(FuncDeclaration, Range));
+  } else if (const auto *ParamDeclaration = dyn_cast<ParamDecl>(Declaration)) {
+    auto Symbols = parseSeparateDeclarationWithRange(ParamDeclaration, Range);
+    SymbolsOrErrorPointer =
+      llvm::make_unique<SymbolsOrError>(std::move(Symbols));
+  } else if (const auto *VariableDeclaration = dyn_cast<VarDecl>(Declaration)) {
+    SingleSymbolOrErrorPointer =
+      llvm::make_unique<SingleSymbolOrError>(parse(VariableDeclaration));
   } else {
     return stringError("unsupported declaration type");
   }
   
-  if (auto Error = SymbolOrError->takeError()) {
-    return std::move(Error);
+  if (SingleSymbolOrErrorPointer) {
+    if (auto Error = SingleSymbolOrErrorPointer->takeError()) {
+      return std::move(Error);
+    }
+    SymbolWithRange Symbol(SingleSymbolOrErrorPointer->get(), Range);
+    return std::vector<SymbolWithRange> { Symbol };
+  } else if (SymbolsOrErrorPointer) {
+    if (auto Error = SymbolsOrErrorPointer->takeError()) {
+      return std::move(Error);
+    }
+    return SymbolsOrErrorPointer->get();
+  } else {
+    return stringError("unsupported declaration type");
   }
-  return SymbolOrError->get();
 }
   
 } //namespace obfuscation
