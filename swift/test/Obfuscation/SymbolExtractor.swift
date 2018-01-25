fb0d26345e401434c2b148f9d2e51ef5466f9147
Jakub Sowa <jakub.sowa@polidea.com>
2018-01-25 16:39:09 +0100

    SymbolExtractor integration tests.
    Reviewers: jerzy.kleszcz, krzysztof.siejkowski
    
    Reviewed By: jerzy.kleszcz, krzysztof.siejkowski
    
    Differential Revision: https://phabricator.polidea.com/D2784

--- swift/test/Obfuscation/SymbolExtractor.swift
+++ swift/test/Obfuscation/SymbolExtractor.swift
@@ -1,2 +1,4 @@
-//RUN: obfuscator-symbol-extractor -filesjson  %S/Inputs/Files.json -symbolsjson %t
-//RUN: diff -w %S/Inputs/ExpectedSymbols.json %t
+//RUN: echo "{\"project\": {\"rootPath\": \"TetsRootPath\"}, \"module\": {\"name\": \"TestModuleName\"}, \"sdk\": {\"name\": \"TestSdkName\", \"path\": \"TestSdkPath\"}, \"filenames\": [\"%S/Inputs\/ViewController.swift\", \"%S/Inputs\/AppDelegate.swift\"], \"explicitelyLinkedFrameworks\": [], \"systemLinkedFrameworks\": []}" > %T/files.json
+//RUN: obfuscator-symbol-extractor -filesjson %T/files.json -symbolsjson %t
+//RUN: diff -w %S/Inputs/expectedSymbols.json %t
+
