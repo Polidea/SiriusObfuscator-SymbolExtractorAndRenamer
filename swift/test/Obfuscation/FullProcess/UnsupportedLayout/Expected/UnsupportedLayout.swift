62584d8a9784d07839cb62e07cc1c1390ea9c3e6
Michal Zielinski <michal.zielinski@polidea.com>
2018-02-16 13:23:19 +0100

    layout files tests
    Summary: unsupported layout features
    
    Reviewers: krzysztof.siejkowski, jerzy.kleszcz
    
    Reviewed By: krzysztof.siejkowski, jerzy.kleszcz
    
    Differential Revision: https://phabricator.polidea.com/D2837

---
+++ swift/test/Obfuscation/FullProcess/UnsupportedLayout/Expected/UnsupportedLayout.swift
@@ -0,0 +1,12 @@
+
+import AppKit
+
+class T1_ViewController {
+  
+  @IBOutlet weak var V1_informativeLabel: UIButton!
+
+  @IBAction func NF1_switchInputRepresentation(_ sender: UIButton) {
+  }
+  
+}
+
