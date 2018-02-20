e5315951e987cfc44b4ec88222b5d97097e0ad8a
Michal Zielinski <michal.zielinski@polidea.com>
2018-02-20 10:44:29 +0100

    renaming outlets and actions in layout files
    Summary:
    obfuscating selectors in xml
    
    updated doc
    
    Reviewers: jerzy.kleszcz, krzysztof.siejkowski
    
    Reviewed By: jerzy.kleszcz, krzysztof.siejkowski
    
    Differential Revision: https://phabricator.polidea.com/D2839

--- swift/test/Obfuscation/FullProcess/LayoutStoryboard/Expected/LayoutStoryboard.swift
+++ swift/test/Obfuscation/FullProcess/LayoutStoryboard/Expected/LayoutStoryboard.swift
@@ -1,3 +1,12 @@
 
-class T1_AppDelegate {}
-class T1_ViewController {}
+import Cocoa
+
+class T1_ViewController {
+  
+  @IBOutlet weak var V1_informativeLabel: NSButton!
+
+  @IBAction func NF1_switchInputRepresentation(_ IP1_sender: NSButton) {
+  }
+  
+}
+
