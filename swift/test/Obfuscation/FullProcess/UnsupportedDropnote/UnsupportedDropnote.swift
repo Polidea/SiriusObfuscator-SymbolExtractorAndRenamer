38b707f74d5f7086de09cd1f09035b8f0082b4ee
Michal Zielinski <michal.zielinski@polidea.com>
2018-02-22 13:50:18 +0100

    additional dropnote failing tests
    Reviewers: jerzy.kleszcz, krzysztof.siejkowski
    
    Reviewed By: jerzy.kleszcz, krzysztof.siejkowski
    
    Differential Revision: https://phabricator.polidea.com/D2850

--- swift/test/Obfuscation/FullProcess/UnsupportedDropnote/UnsupportedDropnote.swift
+++ swift/test/Obfuscation/FullProcess/UnsupportedDropnote/UnsupportedDropnote.swift
@@ -1,7 +1,10 @@
 //XFAIL: *
 //RUN: %target-prepare-obfuscation-for-file "UnsupportedDropnote" %target-run-full-obfuscation
+import Foundation
 
-class Test {}
+class Test {
+  func testFunc() {}
+}
 
 // capture list is not properly renamed - should be?
 class ClosureTest {
@@ -16,9 +19,49 @@ class ClosureTest {
 class Foo {
   var FieldA: String
   var FieldB: String?
-
+  
   init(FieldA: String, FieldB: String? = nil) {}
 }
 let FooObj = Foo(FieldA: "test")
 
+// type is not renamed in if case
+let num = 42
+if case 0...225 = num, num is Test {
+}
 
+// debug blocks are omitted
+final class DebugBlock {
+  fileprivate init() {
+    #if !DEBUG
+      let testInDebug = Test()
+    #endif
+  }
+}
+
+//override init
+class Parent{
+  init(p1: String, p2: Int) {}
+}
+class Child: Parent {
+  override init(p1: String, p2: Int){}
+}
+let c = Child(p1: "p1", p2:42)
+
+//error name in catch block should not be renamed
+func canThrowErrors() throws {}
+func a() {
+  do {
+    try canThrowErrors()
+  } catch {
+    error
+  }
+}
+
+//protocol stuff
+protocol Proto {
+  func hello()
+}
+extension NSString: Proto {}
+extension Proto where Self: NSString {
+  func hello() {}
+}
