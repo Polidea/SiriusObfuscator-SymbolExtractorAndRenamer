cc2b14f2d0aa4676c5cce02788283c7ad2b9a1b6
Michal Zielinski <michal.zielinski@polidea.com>
2018-02-23 15:14:41 +0100

    Exclude implicit variables from obfuscation
    Reviewers: jerzy.kleszcz, krzysztof.siejkowski
    
    Reviewed By: jerzy.kleszcz, krzysztof.siejkowski
    
    Differential Revision: https://phabricator.polidea.com/D2855

--- swift/test/Obfuscation/FullProcess/UnsupportedDropnote/Expected/UnsupportedDropnote.swift
+++ swift/test/Obfuscation/FullProcess/UnsupportedDropnote/Expected/UnsupportedDropnote.swift
@@ -27,15 +27,14 @@ final class T1_DebugBlock {
   }
 }
 
-//error name in catch block should not be renamed
-func NF1_canThrowErrors() throws {}
-func NF1_a() {
-  do {
-    try NF1_canThrowErrors()
-  } catch {
-    error
-  }
+//override init
+class T1_Parent{
+  init(SP1_p1: String, SP1_p2: Int) {}
 }
+class T1_Child: T1_Parent {
+  override init(SP1_p1: String, SP1_p2: Int){}
+}
+let V1_c = T1_Child(SP1_p1: "p1", SP1_p2:42)
 
 //protocol stuff
 protocol T1_Proto {
