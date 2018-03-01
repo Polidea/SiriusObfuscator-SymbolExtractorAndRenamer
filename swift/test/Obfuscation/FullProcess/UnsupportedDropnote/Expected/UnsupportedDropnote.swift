1e91e9b278f3ba590fc88b4677b7578e3d811bec
Jerzy Kleszcz <jerzy.kleszcz@polidea.com>
2018-03-01 12:40:20 +0100

    Renaming type name in `is` cast from optional to non-optional type or vice versa
    Reviewers: michal.zielinski, krzysztof.siejkowski
    
    Reviewed By: michal.zielinski, krzysztof.siejkowski
    
    Subscribers: michal.zielinski, krzysztof.siejkowski
    
    Differential Revision: https://phabricator.polidea.com/D2863

--- swift/test/Obfuscation/FullProcess/UnsupportedDropnote/Expected/UnsupportedDropnote.swift
+++ swift/test/Obfuscation/FullProcess/UnsupportedDropnote/Expected/UnsupportedDropnote.swift
@@ -4,20 +4,6 @@ class T1_Test {
   func NF1_testFunc() {}
 }
 
-// init param FieldA is not renamed if there is a second param with default value
-class T1_Foo {
-  var V1_FieldA: String
-  var V1_FieldB: String?
-  
-  init(SP1_FieldA: String, SP1_FieldB: String? = nil) {}
-}
-let V1_FooObj = T1_Foo(SP1_FieldA: "test")
-
-// type is not renamed in if case
-let V1_num = 42
-if case 0...225 = V1_num, V1_num is T1_Test {
-}
-
 // debug blocks are omitted
 final class T1_DebugBlock {
   fileprivate init() {
